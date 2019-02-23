d4c98d5169168dfc8be967b2e69f36faee1a4ce8
Geri <gregor.horvath83@gmail.com>
2019-02-23 22:17:53 +0200

    Updates

---
+++ precache-manifest.ca4cd74aca30faab87e39d994dafe80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1186cb159b8d39209a1741785de2fa1c",
+    "url": "/weather/static/media/background.1186cb15.jpg"
+  },
+  {
+    "revision": "5346c2795f64f3102fab",
+    "url": "/weather/static/js/runtime~main.5346c279.js"
+  },
+  {
+    "revision": "2432ce45078f1fdd406e",
+    "url": "/weather/static/js/main.2432ce45.chunk.js"
+  },
+  {
+    "revision": "04eae6b29f8c4fd7934c",
+    "url": "/weather/static/js/2.04eae6b2.chunk.js"
+  },
+  {
+    "revision": "2432ce45078f1fdd406e",
+    "url": "/weather/static/css/main.dd5816c4.chunk.css"
+  },
+  {
+    "revision": "f9df101d1e4b48ed01c998e7ffb87627",
+    "url": "/weather/index.html"
+  }
+];
